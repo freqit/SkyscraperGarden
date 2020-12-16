bec78fab71012ba2e8e8c929fbf05d05bc7489ce
Joakim Karlsson <rocktshirts@hotmail.com>
2020-12-16 10:51:38 +0100

    testing movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float speed = 5f;
+    private Vector3 target;
+    private bool move = false;
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            SetTargetPos();
+        }
+
+        if (move)
+        {
+            Move();
+        }
+    }
+
+    private void SetTargetPos()
+    {
+        target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        target.z = transform.position.z;
+        move = true;
+    }
+
+    private void Move()
+    {
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, target);
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if (transform.position == target)
+        {
+            move = false;
+        }
     }
 }
